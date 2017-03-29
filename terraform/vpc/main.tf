b5d48be38aa969e6ddfcfa90adfccc3d2f6fbddf
Nick M <nickmaccarthy@gmail.com>
2017-03-29 07:36:36 -0400

    consul beginnings

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -4,15 +4,18 @@ provider "aws" {
   profile = "hashicorp"
 }
 
-
-resource "aws_vpc" "hasicorp_vpc" {
-  cidr_block = "10.0.20.0/24"
+resource "aws_vpc" "hashicorp_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "hashicorp-vpc"
+  }
 }
 
 resource "aws_security_group" "hashicorp" {
   name = "hashicorp"
   description = "Allow all inbound traffic for the hashicorp code test"
-  vpc_id = "${aws_vpc.hasicorp_vpc.id}"
+  vpc_id = "${aws_vpc.hashicorp_vpc.id}"
 
   ingress {
     from_port   = 0
